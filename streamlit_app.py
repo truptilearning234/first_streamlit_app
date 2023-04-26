8ecc18b7378fdb8cd663a0345c1d1ad8a39c0044
truptilearning234 <130506767+truptilearning234@users.noreply.github.com>
2023-04-26 11:56:14 -0600

    Update streamlit_app.py
    try/except - introduced If/Esle

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,17 +27,17 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api reponse
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
-streamlit.write('The user entered', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-# streamlit.text(fruityvice_response.json())
-
-# take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it on the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  streamlit.error()
 #don't run anything past here while we troubleshoot
 streamlit.stop()
 
